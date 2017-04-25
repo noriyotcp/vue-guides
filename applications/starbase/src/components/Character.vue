ffbf80f9996641542754a1975fd71d01d4386cb0
noriyotcp <noriyo.akita@gmail.com>
2017-04-25 22:54:57 +0900

    Add Character component
    to src/components

---
+++ applications/starbase/src/components/Character.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="">
+    {{character}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      character: {}
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('http://swapi.co/api/people/1', {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(json => this.character = json)
+    }
+  },
+  created() {
+    this.fetchCharacter()
+  }
+}
+</script>
+
+<style lang="css">
+</style>
