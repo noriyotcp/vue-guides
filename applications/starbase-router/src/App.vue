c5ddca7a64483bea3d304bce07d96c359d21f00b
noriyotcp <noriyo.akita@gmail.com>
2017-04-26 14:53:31 +0900

    Introduce Bootstrap and do styling

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1></h1>
-    <h2>Essential Links</h2>
+    <h3>Starbase Router</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -31,30 +31,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
