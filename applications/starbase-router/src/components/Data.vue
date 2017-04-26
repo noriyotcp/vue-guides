d25832812435e60cc4437bbcf247fb7bd6a2ac79
noriyotcp <noriyo.akita@gmail.com>
2017-04-26 17:09:57 +0900

    [Data] Fetch items by id via swapi API

--- applications/starbase-router/src/components/Data.vue
+++ applications/starbase-router/src/components/Data.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="">
-    Type: {{type}}
+    {{items}}
   </div>
 </template>
 
@@ -8,16 +8,30 @@
 export default {
   data() {
     return {
-      type: this.$route.params.type
+      type: this.$route.params.type,
+      items: []
     }
   },
   watch: {
-    '$route': 'change'
+    '$route': 'fetchItems'
   },
   methods: {
-    change() {
+    fetchItems() {
+      this.items = []
       this.type = this.$route.params.type
+      let initial_ids = [1, 13, 14]
+
+      for (let index in initial_ids) {
+        let id = initial_ids[index]
+        console.log('id', id)
+        fetch(`http://swapi.co/api/${this.type}/${id}`, { method: 'GET' })
+          .then(response => response.json())
+          .then(json => this.items.push(json))
+      }
     }
+  },
+  created() {
+    this.fetchItems()
   }
 }
 </script>
