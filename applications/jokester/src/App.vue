66dd05a4fe3bfd83bbaa30f53e496867db1bcc54
noriyotcp <noriyo.akita@gmail.com>
2017-04-28 00:07:56 +0900

    Add Joke component and display a list of jokes

--- applications/jokester/src/App.vue
+++ applications/jokester/src/App.vue
@@ -3,18 +3,30 @@
     <img src="./assets/logo.png">
     <h3>Got jokes?</h3>
     <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
-    {{$store.state}}
+    <br>
+    <br>
+    <div class="col-md-12">
+      <Joke
+        v-for="(joke, index) in $store.state.jokes"
+        :joke="joke"
+        key="index"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
   name: 'app',
   methods: mapActions([
     'initJokes'
-  ])
+  ]),
+  components: {
+    Joke
+  }
 }
 </script>
 
---
+++ applications/jokester/src/Joke.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="col-md-6">
+    <div class="joke-card">
+      <div class="card-block">
+        <p><u>{{joke.setup}}</u></p>
+        <p><em>{{joke.punchline}}</em></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['joke']
+}
+</script>
+
+<style lang="css">
+</style>
