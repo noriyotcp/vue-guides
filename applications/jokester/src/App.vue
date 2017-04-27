9e71d660a16667f42af880f5ae6f8fecdffb96ed
noriyotcp <noriyo.akita@gmail.com>
2017-04-28 01:05:43 +0900

    [App.vue] - Enable to filter jokes by types via checkboxes

--- applications/jokester/src/App.vue
+++ applications/jokester/src/App.vue
@@ -5,10 +5,20 @@
     <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
     <button class="btn btn-primary" @click="addJoke">Add a Joke</button>
     <br>
+    <span v-for="type in types">
+      <input
+        type="checkbox"
+        :value="type"
+        v-model="checkedTypes"
+        checked
+      >
+      <label>{{type}}</label>&nbsp;
+    </span>
     <br>
     <div class="col-md-12">
       <Joke
         v-for="(joke, index) in $store.state.jokes"
+        v-show="checkedTypes.includes(joke.type)"
         :joke="joke"
         :index="index"
         key="index"
@@ -23,6 +33,12 @@ import Joke from './Joke.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      types: ['general', 'knock-knock', 'programming'],
+      checkedTypes: ['general', 'knock-knock', 'programming']
+    }
+  },
   methods: mapActions([
     'initJokes',
     'addJoke'
