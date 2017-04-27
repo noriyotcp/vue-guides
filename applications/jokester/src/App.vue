9cbd6764d59b687b4d64a4fbc4567412aa728ac5
noriyotcp <noriyo.akita@gmail.com>
2017-04-28 00:22:01 +0900

    [App.vue] Map addJoke action to methods and add a random joke by clicking a button

--- applications/jokester/src/App.vue
+++ applications/jokester/src/App.vue
@@ -3,6 +3,7 @@
     <img src="./assets/logo.png">
     <h3>Got jokes?</h3>
     <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+    <button class="btn btn-primary" @click="addJoke">Add a Joke</button>
     <br>
     <br>
     <div class="col-md-12">
@@ -22,7 +23,8 @@ import Joke from './Joke.vue'
 export default {
   name: 'app',
   methods: mapActions([
-    'initJokes'
+    'initJokes',
+    'addJoke'
   ]),
   components: {
     Joke
