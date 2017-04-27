b3187cd533b3fa77cef5d76e9cc5a4cab1259b0f
noriyotcp <noriyo.akita@gmail.com>
2017-04-28 00:39:51 +0900

    Enable to remove the one joke

--- applications/jokester/src/store/mutations.js
+++ applications/jokester/src/store/mutations.js
@@ -7,5 +7,9 @@ export const mutations = {
 
   [types.ADD_JOKE] (state, payload) {
     state.jokes.push(payload)
+  },
+
+  [types.REMOVE_JOKE] (state, index) {
+    state.jokes.splice(index, 1)
   }
 }
