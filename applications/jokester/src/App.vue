0a25955355ecc239054bb76adff3f0737eabb32c
noriyotcp <noriyo.akita@gmail.com>
2017-04-27 16:59:22 +0900

    [App.vue] Map actions to methods and enable to fetch ten jokes by clicking a button

--- applications/jokester/src/App.vue
+++ applications/jokester/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    Got jokes?
+    <h3>Got jokes?</h3>
+    <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
     {{$store.state}}
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  name: 'app'
+  name: 'app',
+  methods: mapActions([
+    'initJokes'
+  ])
 }
 </script>
 
