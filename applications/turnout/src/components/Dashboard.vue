7b6458fcd2647f279066a8827399f6d280f6eb15
noriyotcp <noriyo.akita@gmail.com>
2017-04-28 17:07:12 +0900

    Get rid of app id from Dashboard and Signin

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <h1>Dashboard Component</h1>
   </div>
 </template>
--- applications/turnout/src/components/Signin.vue
+++ applications/turnout/src/components/Signin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <h1>Signin Component</h1>
   </div>
 </template>
