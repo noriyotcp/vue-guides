bdacfa12695029c4069be5704880f8d131d088b1
noriyotcp <noriyo.akita@gmail.com>
2017-04-28 20:37:14 +0900

    [Signin.vue] Create a form for sign-in and a paragraph for error messages

--- applications/turnout/src/components/Signin.vue
+++ applications/turnout/src/components/Signin.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <h1>Signin Component</h1>
+    <div class="form-inline">
+      <h3>Sign In</h3>
+      <div class="form-group">
+        <input
+          type="email"
+          placeholder="email"
+          class="form-control"
+          v-model="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          class="form-control"
+          v-model="password"
+        />
+        <button class="btn btn-primary" @click="signIn">Sign In</button>
+      </div>
+      <br>
+      <p>{{error.message}}</p>
+      <br>
+    </div>
   </div>
 </template>
 
