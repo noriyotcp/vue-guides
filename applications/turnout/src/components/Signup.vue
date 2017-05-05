ad2da76fd8ff450772f85d67cb201b8685b06812
noriyotcp <noriyo.akita@gmail.com>
2017-05-05 12:41:20 +0900

    Add Signup component

--- applications/turnout/src/components/Signup.vue
+++ applications/turnout/src/components/Signup.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="form-inline">
-      <h3>Sign In</h3>
+      <h3>Sign Up</h3>
       <div class="form-group">
         <input
           type="email"
@@ -15,12 +15,12 @@
           class="form-control"
           v-model="password"
         />
-        <button class="btn btn-primary" @click="signIn">Sign In</button>
+        <button class="btn btn-primary" @click="signUp">Sign Up</button>
       </div>
       <br>
       <p>{{error.message}}</p>
       <br>
-      <router-link to="/signup">Not a user? Sign up</router-link>
+      <router-link to="/signin">Already a user? Sign in</router-link>
     </div>
   </div>
 </template>
@@ -29,7 +29,7 @@
 import { firebaseApp } from '../firebaseApp'
 
 export default {
-  name: 'signin',
+  name: 'signup',
   data() {
     return {
       email: '',
@@ -40,8 +40,8 @@ export default {
     }
   },
   methods: {
-    signIn() {
-      firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+    signUp() {
+      firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
         .catch(error => {
           this.error = error
         })
--- applications/turnout/src/main.js
+++ applications/turnout/src/main.js
@@ -8,12 +8,14 @@ import store from './store'
 
 import Dashboard from './components/Dashboard.vue'
 import Signin from './components/Signin.vue'
+import Signup from './components/Signup.vue'
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/dashboard', component: Dashboard },
-    { path: '/signin', component: Signin }
+    { path: '/signin', component: Signin },
+    { path: '/signup', component: Signup }
   ]
 })
 
